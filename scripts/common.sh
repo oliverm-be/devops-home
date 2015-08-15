c128b9ae68a46cbc43e535e411daec906a5b1b11
Oliver May <oliver@levvie.be>
2015-08-15 13:25:47 +0200

    config file and prerequisites

---
+++ scripts/common.sh
@@ -0,0 +1,12 @@
+#!=/bin/bash
+# Global config file
+raspberrypi="raspberrypi.home.levvie.be"
+raspberrypi_mac="b8:27:eb:1a:86:3c"
+
+homeserver="homeserver.home.levvie.be"
+homeserver_mac="ec:a8:6b:ff:d2:70"
+
+nas="nas-kel-oli.home.levvie.be"
+nas_mac="a0:21:b7:c1:6c:9c"
+
+cloud="cloud.levvie.be"
